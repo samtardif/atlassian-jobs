009b278828ebfa49e418077de3c8f36fecd34d7c
cllo7748 <cllo7748@c7568b54-67a9-0410-839d-a83ee3309b6c>
2011-08-19 10:59:21 +0000

    Move everything into trunk.
    git-svn-id: http://svn.ug.it.usyd.edu.au/svn/info3600_11_y/trunk@5 c7568b54-67a9-0410-839d-a83ee3309b6c

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtlassianJobs::Application.config.secret_token = 'a11952875f739bb32a86c8e2a5b394bd24fcea8e0d0ba48312c0ab1ec035fbdfdeda55a8214a86a071a189ea8083a7a16f22636c2aee591197adaff42876244b'
